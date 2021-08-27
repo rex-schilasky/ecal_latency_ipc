bb31c7ef5bd4a76075a3620aba8236433fb18985
rex-schilasky <49162693+rex-schilasky@users.noreply.github.com>
2021-08-27 14:26:06 +0200

    buffer count argument add

--- latency_snd/src/latency_snd.cpp
+++ latency_snd/src/latency_snd.cpp
@@ -14,13 +14,14 @@ long long get_microseconds()
 }
 
 // single test run
-void do_run(const int runs, int snd_size /*kB*/, int delay /*ms*/, bool zero_copy)
+void do_run(const int runs, int snd_size /*kB*/, int delay /*ms*/, int mem_buffer, bool zero_copy)
 {
   // log parameter
   std::cout << "--------------------------------------------"    << std::endl;
   std::cout << "Runs                    : " << runs              << std::endl;
   std::cout << "Message size            : " << snd_size << " kB" << std::endl;
   std::cout << "Message delay           : " << delay    << " ms" << std::endl;
+  std::cout << "Memory buffer           : " << mem_buffer        << std::endl;
   if (zero_copy)
   {
     std::cout << "Zero copy               : ON"  << std::endl;
@@ -35,6 +36,9 @@ void do_run(const int runs, int snd_size /*kB*/, int delay /*ms*/, bool zero_cop
 
   // create publisher and subscriber
   eCAL::CPublisher pub("ping");
+
+  // set number of publisher memory buffers
+  pub.SetBufferCount(mem_buffer);
 
   // enable zero copy mode
   pub.EnableZeroCopy(zero_copy);
@@ -72,25 +76,27 @@ int main(int argc, char **argv)
   {
     // parse command line
     TCLAP::CmdLine cmd("latency_snd");
-    TCLAP::ValueArg<int> runs     ("r", "runs",      "Number of messages to send.", false, 1000, "int");
-    TCLAP::ValueArg<int> size     ("s", "size",      "Messages size in kB.",        false, -1,   "int");
-    TCLAP::ValueArg<int> delay    ("d", "delay",     "Messages send delay in ms.",  false, 50,   "int");
-    TCLAP::SwitchArg     zero_copy("z", "zero_copy", "Switch zero copy mode on.");
+    TCLAP::ValueArg<int> runs      ("r", "runs",       "Number of messages to send.",            false, 1000, "int");
+    TCLAP::ValueArg<int> size      ("s", "size",       "Messages size in kB.",                   false,   -1, "int");
+    TCLAP::ValueArg<int> delay     ("d", "delay",      "Messages send delay in ms.",             false,   50, "int");
+    TCLAP::ValueArg<int> mem_buffer("b", "mem_buffer", "Number of memory files per connection.", false,    1, "int");
+    TCLAP::SwitchArg     zero_copy ("z", "zero_copy",  "Switch zero copy mode on.");
     cmd.add(runs);
     cmd.add(size);
     cmd.add(delay);
+    cmd.add(mem_buffer);
     cmd.add(zero_copy);
     cmd.parse(argc, argv);
 
     if(size < 0)
     {
       // automatic size mode
-      for (int s = 1; s <= 16384; s *= 2) do_run(runs.getValue(), s, delay.getValue(), zero_copy.getValue());
+      for (int s = 1; s <= 16384; s *= 2) do_run(runs.getValue(), s, delay.getValue(), mem_buffer.getValue(), zero_copy.getValue());
     }
     else
     {
       // run single test
-      do_run(runs.getValue(), size.getValue(), delay.getValue(), zero_copy.getValue());
+      do_run(runs.getValue(), size.getValue(), delay.getValue(), mem_buffer.getValue(), zero_copy.getValue());
     }
   }
   catch (TCLAP::ArgException &e)  // catch any exceptions
