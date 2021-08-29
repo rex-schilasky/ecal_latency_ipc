6cc9d6dd0807545ad02bed8328ab4663832de071
rex-schilasky <49162693+rex-schilasky@users.noreply.github.com>
2021-08-29 08:05:52 +0200

    logging excluded in separate module

--- latency_rec/src/latency_rec.cpp
+++ latency_rec/src/latency_rec.cpp
@@ -1,23 +1,15 @@
 #include <ecal/ecal.h>
 
-#include <algorithm>
 #include <chrono>
-#include <fstream>
 #include <iostream>
-#include <iterator>
 #include <mutex>
-#include <numeric>
-#include <vector>
-
+#include <thread>
 #include <tclap/CmdLine.h>
+
+#include "latency_log.h"
 
 // warmup runs not to measure
 const int warmups(10);
-
-// helper
-long long get_microseconds();
-void evaluate(std::vector<long long>& lat_arr_, size_t rec_size_, size_t warmups_, std::string& log_file_);
-void log2file(std::vector<long long>& lat_arr_, size_t rec_size_, std::string& log_file_);
 
 // data structure for later evaluation
 struct SCallbackPar
@@ -33,7 +25,7 @@ struct SCallbackPar
 void on_receive(const struct eCAL::SReceiveCallbackData* data_, SCallbackPar* par_, int delay_, std::string& log_file_)
 {
   // get receive time stamp
-  auto rec_time = get_microseconds();
+  auto rec_time = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 
   // update latency, size and msg number
   std::lock_guard<std::mutex> lock(par_->mtx);
@@ -41,7 +33,7 @@ void on_receive(const struct eCAL::SReceiveCallbackData* data_, SCallbackPar* pa
   par_->rec_size = data_->size;
   par_->msg_num++;
 
-  if(delay_ > 0) eCAL::Process::SleepMS(delay_);
+  if(delay_ > 0) std::this_thread::sleep_for(std::chrono::milliseconds(delay_));
 }
 
 // single test run
@@ -68,7 +60,7 @@ void do_run(int delay_, std::string& log_file_)
       if ((cb_par.msg_num > 0) && (msg_last == cb_par.msg_num)) break;
       else msg_last = cb_par.msg_num;
     }
-    eCAL::Process::SleepMS(1000);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   }
 
   // detach callback
@@ -106,66 +98,4 @@ int main(int argc, char** argv)
   }
 
   return(0);
-}
-
-// time getter
-long long get_microseconds()
-{
-  std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
-  return(std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch()).count());
-}
-
-// evaluation
-void evaluate(std::vector<long long>& lat_arr_, size_t rec_size_, size_t warmups_, std::string& log_file_)
-{
-  std::stringstream ss;
-
-  // remove warmup runs
-  if (lat_arr_.size() >= warmups_)
-  {
-    lat_arr_.erase(lat_arr_.begin(), lat_arr_.begin() + warmups_);
-  }
-
-  // evaluate all
-  size_t sum_msg = lat_arr_.size();
-  ss << "--------------------------------------------" << std::endl;
-  ss << "Messages received             : " << sum_msg  << std::endl;
-  if (sum_msg > warmups_)
-  {
-    long long sum_time = std::accumulate(lat_arr_.begin(), lat_arr_.end(), 0LL);
-    long long avg_time = sum_time / sum_msg;
-    auto      min_it = std::min_element(lat_arr_.begin(), lat_arr_.end());
-    auto      max_it = std::max_element(lat_arr_.begin(), lat_arr_.end());
-    size_t    min_pos = min_it - lat_arr_.begin();
-    size_t    max_pos = max_it - lat_arr_.begin();
-    long long min_time = *min_it;
-    long long max_time = *max_it;
-    ss << "Message size received         : " << rec_size_ / 1024 << " kB"       << std::endl;
-    ss << "Message average latency       : " << avg_time << " us"               << std::endl;
-    ss << "Message min latency           : " << min_time << " us @ " << min_pos << std::endl;
-    ss << "Message max latency           : " << max_time << " us @ " << max_pos << std::endl;
-    ss << "Throughput                    : " << static_cast<int>(((rec_size_ * sum_msg) / 1024.0) / (sum_time / 1000.0 / 1000.0))          << " kB/s"  << std::endl;
-    ss << "                              : " << static_cast<int>(((rec_size_ * sum_msg) / 1024.0 / 1024.0) / (sum_time / 1000.0 / 1000.0)) << " MB/s"  << std::endl;
-    ss << "                              : " << static_cast<int>(sum_msg / (sum_time / 1000.0 / 1000.0))                                   << " Msg/s" << std::endl;
-  }
-  ss << "--------------------------------------------" << std::endl;
-
-  // log to console
-  std::cout << ss.str();
-
-  // log into logfile (append)
-  std::ofstream ofile;
-  ofile.open(log_file_, std::ios::out | std::ios::app);
-  ofile << ss.str();
-}
-
-void log2file(std::vector<long long>& lat_arr_, size_t rec_size_, std::string& log_file_)
-{
-  std::stringstream ss;
-  ss << std::setw(6) << std::setfill('0') << rec_size_/1024;
-  std::string rec_size_s = ss.str();
-
-  std::ofstream ofile(rec_size_s + "-" +  log_file_);
-  std::ostream_iterator<long long>output_iterator(ofile, "\n");
-  std::copy(lat_arr_.begin(), lat_arr_.end(), output_iterator);
 }
